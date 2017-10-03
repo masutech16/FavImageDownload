7581032be9cbbae4d834bb9cfac2371aa0cc209d
Ikeno Masaki <leafclover64@gmail.com>
2017-10-03 14:30:31 +0900

    フォルダを作成しようとすると動かない

--- src/FavImageDownload/DriveWrapper.java
+++ src/FavImageDownload/DriveWrapper.java
@@ -3,6 +3,7 @@ package FavImageDownload;
 
 import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.media.MediaHttpUploader;
 import com.google.api.client.http.FileContent;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -10,8 +11,11 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
+import com.google.api.services.drive.model.ParentReference;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 
 /**
@@ -35,18 +39,23 @@ public class DriveWrapper {
                 .build()
                 .setFromTokenResponse(response);
 
-        drive = new Drive.Builder(httpTransport, jsonFactory, credential).build();
+        drive = new Drive.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("FavImageDownload").build();
     }
 
     public void storeImage(java.io.File file) {
-        File body = new File().setTitle("test");
+        File parent = new File().setTitle("dir")
+                .setMimeType("application/vnd.google-apps.folder"); //driveのREST API参照
+        File metaData = new File().setTitle("test")
+                .setParents(Arrays.asList(new ParentReference().setId(parent.getId())));
         FileContent mediaContent = new FileContent(null,file);
         try {
-            drive.files().insert(body,mediaContent).execute();
+        //    drive.files().insert(parent).execute();
+            drive.files().insert(metaData,mediaContent).execute();
         } catch (Exception e) {
             System.out.println("driveの書き込み中にエラーが発生しました");
             e.printStackTrace();
         }
-
     }
+
 }
