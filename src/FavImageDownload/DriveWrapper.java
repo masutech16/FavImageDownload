f0b53b6b10c337b75a58350d02491ac03f6963e2
Ikeno Masaki <leafclover64@gmail.com>
2017-10-07 16:58:33 +0900

    ディレクトリが複数できる問題を解決

--- src/FavImageDownload/DriveWrapper.java
+++ src/FavImageDownload/DriveWrapper.java
@@ -10,9 +10,13 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.google.api.services.drive.model.ParentReference;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -21,6 +25,10 @@ import java.util.Arrays;
 public class DriveWrapper implements IFileUpload {
 
     private Drive drive;
+    private final static String FOLDER = "application/vnd.google-apps.folder";
+    private final static String PHOTO = "image/jpeg";
+    private final static String FOLDER_NAME = "FavImages";
+
 
     public DriveWrapper() {
         HttpTransport httpTransport = new NetHttpTransport();
@@ -41,25 +49,48 @@ public class DriveWrapper implements IFileUpload {
     }
 
     @Override
-    //FIXME:毎回ディレクトリが作成されてしまう
     public void uploadImage(IImage image) {
-        if(!(image.exist())) {
+        if (!(image.exist())) {
             System.out.println("指定されたファイルは存在しません");
             return;
         }
         try {
-            File parent = new File().setTitle("test")
-                    .setMimeType("application/vnd.google-apps.folder");
-            parent = drive.files().insert(parent).execute();
-
+            File parent = getParentDirectory(FOLDER_NAME);
             File metaData = new File().setTitle(image.getTitle())
-                    .setMimeType("application/vnd.google-apps.photo")
+                    .setMimeType(PHOTO)
                     .setParents(Arrays.asList(new ParentReference().setId(parent.getId())));
             FileContent mediaContent = new FileContent(null, image.getFile());
-            drive.files().insert(metaData,mediaContent).execute();
+            drive.files().insert(metaData, mediaContent).execute();
         } catch (Exception e) {
             System.out.println("driveの書き込み中にエラーが発生しました");
             e.printStackTrace();
         }
+    }
+
+    //root直下のディレクトリを探します
+    //将来的には任意のパスをとれるようにしたい
+    private File getParentDirectory(String parentPath) throws IOException {
+        List<File> children = searchAllFiles(drive.files().list().setQ("trashed = false"));
+        for (File file : children) {
+            if (file.getTitle().equals(parentPath)) return file;
+        }
+        File parent = new File()
+                .setTitle(parentPath)
+                .setMimeType(FOLDER);
+        parent = drive.files().insert(parent).execute();
+        return parent;
+    }
+
+    private List<File> searchAllFiles(Drive.Files.List request) throws IOException {
+        List<File> result = new ArrayList<>();
+
+        do {
+            FileList files = request.execute();
+            result.addAll(files.getItems());
+            request.setPageToken(files.getNextPageToken());
+        }
+        while (request.getPageToken() != null && request.getPageToken().length() > 0);
+
+        return result;
     }
 }
